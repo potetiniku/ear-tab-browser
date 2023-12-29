a18e01658094e8bac19dd5f58a4f883186c460a1
potetiniku <potetiniku@gmail.com>
2023-12-29 13:24:46 +0900

    ボタンの動作を実装

--- EarTabBrowser/EarTabBrowser.Wpf/Browser.xaml.cs
+++ EarTabBrowser/EarTabBrowser.Wpf/Browser.xaml.cs
@@ -22,5 +22,46 @@ public partial class Browser : UserControl
 	public Browser()
 	{
 		InitializeComponent();
+		_ = InitializeWebViewAsync();
 	}
+
+	private async Task InitializeWebViewAsync()
+	{
+		await webView.EnsureCoreWebView2Async(null);
+
+		webView.CoreWebView2.WebMessageReceived +=
+			(_, e) => addressBar.Text = e.TryGetWebMessageAsString();
+
+		await webView.CoreWebView2.AddScriptToExecuteOnDocumentCreatedAsync(
+			"window.chrome.webview.postMessage(window.document.URL);");
+	}
+
+	private void NavigateIfRetrun(object sender, KeyEventArgs e)
+	{
+		if (e.Key == Key.Return &&
+			webView != null && webView.CoreWebView2 != null &&
+			(addressBar.Text.StartsWith("http://") || addressBar.Text.StartsWith("https://")))
+			webView.CoreWebView2.Navigate(addressBar.Text);
+	}
+
+	private void GoBack(object sender, RoutedEventArgs e) => webView.CoreWebView2.GoBack();
+	private void GoForward(object sender, RoutedEventArgs e) => webView.CoreWebView2.GoForward();
+	private void Reload(object sender, RoutedEventArgs e) => webView.CoreWebView2.Reload();
+	private void MinimizeWindow(object sender, RoutedEventArgs e) => Window.GetWindow(this).WindowState = WindowState.Minimized;
+
+	private void MaximizeWindow(object sender, RoutedEventArgs e)
+	{
+		Window mainWindow = Window.GetWindow(this);
+		switch (mainWindow.WindowState)
+		{
+			case WindowState.Normal:
+				mainWindow.WindowState = WindowState.Maximized;
+				break;
+			case WindowState.Maximized:
+				mainWindow.WindowState = WindowState.Normal;
+				break;
+		}
+	}
+
+	private void CloseWindow(object sender, RoutedEventArgs e) => Window.GetWindow(this).Close();
 }
